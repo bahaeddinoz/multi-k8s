92d9c4e2c6b6c6e85539ff202cb77350fe94b83d
vagrant <vagrant@controller.test>
2020-05-26 13:41:54 +0000

    change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
-docker build -t bahaoz/multi-client:latest -t bahaoz/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t bahaoz/multi-server:latest -t bahaoz/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t bahaoz/multi-worker:latest -t bahaoz/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t bahaeddinoz/multi-client:latest -t bahaeddinoz/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t bahaeddinoz/multi-server:latest -t bahaeddinoz/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t bahaeddinoz/multi-worker:latest -t bahaeddinoz/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push bahaoz/multi-client:latest
-docker push bahaoz/multi-server:latest
-docker push bahaoz/multi-worker:latest
+docker push bahaeddinoz/multi-client:latest
+docker push bahaeddinoz/multi-server:latest
+docker push bahaeddinoz/multi-worker:latest
 
-docker push bahaoz/multi-client:$SHA
-docker push bahaoz/multi-server:$SHA
-docker push bahaoz/multi-worker:$SHA
+docker push bahaeddinoz/multi-client:$SHA
+docker push bahaeddinoz/multi-server:$SHA
+docker push bahaeddinoz/multi-worker:$SHA
 
 kubectl apply -f k8s
 
-kubectl set image deployments/server-deployment server=bahaoz/multi-server:$SHA
-kubectl set image deployments/client-deployment client=bahaoz/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=bahaoz/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=bahaeddinoz/multi-server:$SHA
+kubectl set image deployments/client-deployment client=bahaeddinoz/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=bahaeddinoz/multi-worker:$SHA
 
